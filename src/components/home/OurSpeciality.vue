98f54afc44dff2bc05e41295476c635c44c1b99a
neprostostas <neprostostas.gg@gmail.com>
2023-07-31 14:20:35 +0300

    responsive layout for Our Speciality block & 3-grid Block

--- src/components/home/OurSpeciality.vue
+++ src/components/home/OurSpeciality.vue
@@ -11,13 +11,13 @@
       </div>
     </div>
 
-    <div class="bottom-wrap">
+    <div class="bottom-wrap desktop">
       <hr>
       <div class="bottom-block" :style="{'grid-template-columns': bottomBlockColumns }">
-        <div class="block" @mouseover="onHover(1)" @mouseleave="onHoverEnd(1)" :style="{'gap': gap1 }">
+        <div class="block" @mouseover="onHover(1)" :style="{'gap': gap1 }">
           <transition name="fade">
             <img
-                v-if="hoveredBlock === 1 && showImage"
+                v-if="hoveredBlock === 1"
                 src="@/assets/img/home/home-slide1.png"
                 alt="home-slide1"
             />
@@ -28,10 +28,10 @@
           </div>
         </div>
 
-        <div class="block" @mouseover="onHover(2)" @mouseleave="onHoverEnd(2)" :style="{'gap': gap2 }">
+        <div class="block" @mouseover="onHover(2)" :style="{'gap': gap2 }">
           <transition name="fade">
             <img
-                v-if="hoveredBlock === 2 && showImage"
+                v-if="hoveredBlock === 2"
                 src="@/assets/img/home/home-slide2.png"
                 alt="home-slide2"
             />
@@ -42,14 +42,43 @@
           </div>
         </div>
 
-        <div class="block" @mouseover="onHover(3)" @mouseleave="onHoverEnd(3)" :style="{'gap': gap3 }">
+        <div class="block" @mouseover="onHover(3)" :style="{'gap': gap3 }">
           <transition name="fade">
             <img
-                v-if="hoveredBlock === 3 && showImage"
+                v-if="hoveredBlock === 3"
                 src="@/assets/img/home/home-slide3.png"
                 alt="home-slide3"
             />
           </transition>
+          <div class="text-part">
+            <h2 class="heading-2 color-primary">Tech Support & Database Warehouse</h2>
+            <p class="color-primary">Keep your systems running like clockwork with our reliable tech support services. We offer timely assistance and maintenance to ensure that your tech infrastructure stays robust and efficient. Additionally, our secure database warehouse solutions provide a seamless data management experience, empowering you to make data-driven decisions with confidence.</p>
+          </div>
+        </div>
+      </div>
+      <hr>
+    </div>
+    <div class="bottom-wrap mobile">
+      <hr>
+      <div class="bottom-block">
+        <div class="block">
+          <img src="@/assets/img/home/home-slide1.png" alt="home-slide1"/>
+          <div class="text-part">
+            <h2 class="heading-2 color-primary">Web Development</h2>
+            <p class="color-primary">Unleash the true potential of your online presence with our bespoke web development services. Our talented team crafts visually captivating, user-friendly, and fully optimized websites that not only reflect your brand identity but also drive tangible results.</p>
+          </div>
+        </div>
+
+        <div class="block">
+          <img src="@/assets/img/home/home-slide2.png" alt="home-slide2"/>
+          <div class="text-part">
+            <h2 class="heading-2 color-primary">Mobile App Creation</h2>
+            <p class="color-primary">In the era of mobile-first experiences, we deliver top-of-the-line mobile app solutions that captivate users and elevate your business to new heights. Our mobile app development expertise spans across diverse platforms, ensuring seamless functionality and exceptional user engagement.</p>
+          </div>
+        </div>
+
+        <div class="block">
+          <img src="@/assets/img/home/home-slide3.png" alt="home-slide3" />
           <div class="text-part">
             <h2 class="heading-2 color-primary">Tech Support & Database Warehouse</h2>
             <p class="color-primary">Keep your systems running like clockwork with our reliable tech support services. We offer timely assistance and maintenance to ensure that your tech infrastructure stays robust and efficient. Additionally, our secure database warehouse solutions provide a seamless data management experience, empowering you to make data-driven decisions with confidence.</p>
@@ -63,72 +92,48 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import {defineComponent, ref} from "vue";
 
 export default defineComponent({
   name: "OurSpeciality",
   setup() {
     const hoveredBlock = ref(1);
-    const showImage = ref(true);
     const bottomBlockColumns = ref("2fr 1fr 1fr");
     const gap1 = ref("20px");
     const gap2 = ref("0");
     const gap3 = ref("0");
 
+    const changeLayout = (blockNumber) => {
+      if (blockNumber === 1) {
+        bottomBlockColumns.value = "2fr 1fr 1fr";
+        gap1.value = "20px";
+        gap2.value = "0";
+        gap3.value = "0";
+      }
+      else if (blockNumber === 2) {
+        bottomBlockColumns.value = "1fr 2fr 1fr";
+        gap1.value = "0";
+        gap2.value = "20px";
+        gap3.value = "0";
+
+      }
+      else if (blockNumber === 3) {
+        bottomBlockColumns.value = "1fr 1fr 2fr";
+        gap1.value = "0";
+        gap2.value = "0";
+        gap3.value = "20px";
+      }
+    }
+
     const onHover = (blockNumber) => {
       hoveredBlock.value = blockNumber;
-      showImage.value = true;
-      if (blockNumber === 1) {
-        bottomBlockColumns.value = "2fr 1fr 1fr";
-        gap1.value = "20px";
-        gap2.value = "0";
-        gap3.value = "0";
-      }
-      else if (blockNumber === 2) {
-        bottomBlockColumns.value = "1fr 2fr 1fr";
-        gap1.value = "0";
-        gap2.value = "20px";
-        gap3.value = "0";
-
-      }
-      else if (blockNumber === 3) {
-        bottomBlockColumns.value = "1fr 1fr 2fr";
-        gap1.value = "0";
-        gap2.value = "0";
-        gap3.value = "20px";
-      }
-    };
-
-    const onHoverEnd = (blockNumber) => {
-      hoveredBlock.value = blockNumber;
-      showImage.value = true;
-      if (blockNumber === 1) {
-        bottomBlockColumns.value = "2fr 1fr 1fr";
-        gap1.value = "20px";
-        gap2.value = "0";
-        gap3.value = "0";
-      }
-      else if (blockNumber === 2) {
-        bottomBlockColumns.value = "1fr 2fr 1fr";
-        gap1.value = "0";
-        gap2.value = "20px";
-        gap3.value = "0";
-
-      }
-      else if (blockNumber === 3) {
-        bottomBlockColumns.value = "1fr 1fr 2fr";
-        gap1.value = "0";
-        gap2.value = "0";
-        gap3.value = "20px";
-      }
+      changeLayout(blockNumber);
     };
 
     return {
       hoveredBlock,
-      showImage,
       bottomBlockColumns,
       onHover,
-      onHoverEnd,
       gap1,
       gap2,
       gap3
@@ -145,7 +150,9 @@ export default defineComponent({
 .block :is(h2, p) {
   display: grid;
   align-self: start;
+}
 
+.bottom-wrap.desktop .block :is(h2, p) {
   width: 315px;
 }
 
@@ -155,14 +162,14 @@ export default defineComponent({
   display: grid;
   grid-template-rows: 125px 1fr;
   align-items: center;
-  overflow-y: scroll;
+  overflow: scroll;
 }
 
 div.bottom-wrap > div > div:nth-child(3) > .text-part {
   margin-right: 0;
 }
 
-.bottom-wrap {
+.bottom-wrap.desktop {
   padding: 0 20px;
   display: grid;
   grid-template-columns: repeat(3, auto);
@@ -220,15 +227,59 @@ div.bottom-wrap > div > div:nth-child(3) > .text-part {
 .top-block {
   display: grid;
   grid-template-columns: repeat(2, auto);
-  align-items: center;
+  align-items: start;
   justify-content: space-between;
-  gap: 20px;
+  gap: 50px;
   padding-bottom: 100px;
 }
 
 .our-speciality {
   margin-top: 200px;
   margin-bottom: 200px;
+}
+
+.bottom-wrap.mobile {
+  display: none;
+  grid-template-columns: auto 1fr auto;
+}
+
+.bottom-wrap.mobile .bottom-block {
+  grid-template-columns: auto;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+.bottom-wrap.mobile .block {
+  grid-template-columns: auto;
+  gap: 25px;
+  padding: 0 0 20px;
+  border-left: none;
+  border-right: none;
+  border-top: none;
+  border-bottom: var(--color-separator, rgba(35, 35, 35, 0.60)) 1px solid;
+}
+
+.bottom-wrap.mobile .block h2 {
+  padding-top: 0;
+}
+
+.bottom-wrap.mobile .block p {
+  max-width: unset;
+}
+
+.bottom-wrap.mobile .text-part {
+  margin-right: 0;
+  height: unset;
+  overflow: auto;
+  grid-template-rows: 95px 1fr;
+}
+
+.bottom-wrap.mobile .block:nth-child(3) {
+  border-bottom: none;
+}
+
+.bottom-wrap.mobile .block img {
+  height: unset;
 }
 
 /* Fade-in animation */
@@ -244,24 +295,95 @@ div.bottom-wrap > div > div:nth-child(3) > .text-part {
 
 @media screen and (max-width: 1675px) {
 
-  .block:nth-child(3) h2 {
-      font-size: 30px;
+  .bottom-wrap.desktop .block:nth-child(3) h2 {
+      font-size: 25px;
+  }
+
+}
+
+@media screen and (max-width: 1620px) {
+
+  .bottom-wrap.desktop .block:is(:nth-child(1), :nth-child(2)) h2 {
+    max-width: 170px;
+    font-size: 30px;
+  }
+
+  .bottom-wrap.desktop .block:nth-child(3) h2 {
+    max-width: 235px;
+  }
+
+  .bottom-wrap.desktop .block p {
+    max-width: 270px;
   }
 
 }
 
 @media screen and (max-width: 1530px) {
 
-  .block:nth-child(3) h2 {
+  .bottom-wrap.desktop .block:nth-child(3) h2 {
     font-size: 28px;
   }
 
 }
 
 @media screen and (max-width: 1180px) {
-  .bottom-block {
-    grid-template-columns: auto!important;
+
+  .bottom-wrap.desktop {
+    display: none;
   }
+
+  .bottom-wrap.mobile {
+    display: grid;
+  }
+
+  .top-block {
+    grid-template-columns: auto;
+    justify-content: unset;
+    padding-bottom: 50px;
+    gap: 25px;
+  }
+
+  .our-speciality {
+    margin-top: 85px;
+    margin-bottom: 85px;
+  }
+
+  .w-840 {
+    max-width: unset;
+  }
+
+}
+
+@media screen and (max-width: 920px) {
+
+  .pl-225 {
+    padding-left: 150px;
+  }
+
+}
+
+@media screen and (max-width: 460px) {
+
+  .pl-225 {
+    padding-left: 90px;
+  }
+
+}
+
+@media screen and (max-width: 425px) {
+
+  .bottom-wrap.mobile > div > div:nth-child(3) > .text-part {
+    grid-template-rows: 150px 1fr;
+  }
+}
+
+
+@media screen and (max-width: 390px) {
+
+  .pl-225 {
+    padding-left: 70px;
+  }
+
 }
 
 </style>
