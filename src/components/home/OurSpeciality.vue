c79c4411ff70cc34b5e079c319afb5c13e526ba8
neprostostas <neprostostas.gg@gmail.com>
2023-08-03 17:03:24 +0300

    fix

--- src/components/home/OurSpeciality.vue
+++ src/components/home/OurSpeciality.vue
@@ -253,7 +253,7 @@ div.bottom-wrap > div > div:nth-child(3) > .text-part {
 
 .bottom-wrap.mobile .block {
   grid-template-columns: auto;
-  grid-template-rows: 1fr auto;
+  grid-template-rows: minmax(310px, auto) auto;
   gap: 25px;
   padding: 20px 0;
   border-left: none;
